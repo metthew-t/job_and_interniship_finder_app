e6ccfaaeda2d1c4adf83be30ec276ebdc5b70b44
Matthew Teferi <metthewsteferi@gmail.com>
2026-06-01 18:13:07 -0700

    Force strict database column mapping

--- backend/models/Profile.js
+++ backend/models/Profile.js
@@ -3,21 +3,22 @@ const sequelize = require('../config/database');
 
 const Profile = sequelize.define('Profile', {
   university: { type: DataTypes.STRING },
-  courseOfStudy: { type: DataTypes.STRING },
+  courseOfStudy: { type: DataTypes.STRING, field: 'course_of_study' },
   bio: { type: DataTypes.TEXT },
-  resumeUrl: { type: DataTypes.STRING },
-  locationPreference: { type: DataTypes.STRING },
-  yearsOfExperience: { type: DataTypes.INTEGER },
-  expectedSalary: { type: DataTypes.INTEGER },
+  resumeUrl: { type: DataTypes.STRING, field: 'resume_url' },
+  locationPreference: { type: DataTypes.STRING, field: 'location_preference' },
+  yearsOfExperience: { type: DataTypes.INTEGER, field: 'years_of_experience' },
+  expectedSalary: { type: DataTypes.INTEGER, field: 'expected_salary' },
   skills: { type: DataTypes.STRING },
   profileCompletionPercentage: {
     type: DataTypes.INTEGER,
-    defaultValue: 0
+    defaultValue: 0,
+    field: 'profile_completion_percentage'
   }
 }, {
   tableName: 'profiles',
   timestamps: true,
-  underscored: true // This converts courseOfStudy -> course_of_study in DB
+  underscored: true
 });
 
 module.exports = Profile;
--- backend/models/User.js
+++ backend/models/User.js
@@ -3,15 +3,15 @@ const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  passwordHash: { type: DataTypes.STRING },
+  passwordHash: { type: DataTypes.STRING, field: 'password_hash' },
   role: { type: DataTypes.STRING, allowNull: false, defaultValue: 'student' },
-  firstName: { type: DataTypes.STRING },
-  lastName: { type: DataTypes.STRING },
-  isVerified: { type: DataTypes.BOOLEAN, defaultValue: false }
+  firstName: { type: DataTypes.STRING, field: 'first_name' },
+  lastName: { type: DataTypes.STRING, field: 'last_name' },
+  isVerified: { type: DataTypes.BOOLEAN, defaultValue: false, field: 'is_verified' }
 }, {
   tableName: 'users',
   timestamps: true,
-  underscored: true // This converts firstName -> first_name in DB
+  underscored: true
 });
 
 module.exports = User;
